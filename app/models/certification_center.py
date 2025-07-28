d77d3c4fd2cf1408c170069026d4fa435b80c07a
bobojonov-sobirjon <sobirbobojonov05763104@gmail.com>
2025-07-28 06:09:56 +0500

    changed

--- app/models/certification_center.py
+++ app/models/certification_center.py
@@ -3,7 +3,7 @@ from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from ..database import Base
 
-class CertificwationCenter(Base):
+class CertificationCenter(Base):
     __tablename__ = "certification_centers"
 
     id = Column(Integer, primary_key=True, index=True)
