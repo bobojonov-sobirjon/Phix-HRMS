43445b1633912260a4ef34dec9ad4fcd8d95033b
bobojonov-sobirjon <sobirbobojonov05763104@gmail.com>
2025-08-04 16:54:53 +0500

    modified

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,23 @@
-from .auth import router as auth_router
-from .faq import *
-from .contact_us import *
-from .languages import router as languages_router
+from .auth import router as auth
+from .roles import router as roles
+from .locations import router as locations
+from .skills import router as skills
+from .profile import router as profile
+from .user_skills import router as user_skills
+from .faq import router as faq
+from .contact_us import router as contact_us
+from .languages import router as languages
+from .data_management import router as data_management
 
-__all__ = ["auth_router"] 
+__all__ = [
+    "auth",
+    "roles", 
+    "locations",
+    "skills",
+    "profile",
+    "user_skills",
+    "faq",
+    "contact_us",
+    "languages",
+    "data_management"
+] 
